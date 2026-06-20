4d4ccef39d7ec55fbd3548fc30d1a9d4bfac6afb
NikitaKadrame04 <nikitamk2912@gmail.com>
2026-06-20 18:14:38 +0530

    Chapter1: Pointers & References

--- Working/Chapt1.cpp
+++ Working/Chapt1.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 
 int main() {
-    int i {47};
-    cout<< "i: " << i << endl;
+//Pointers
+    int x {45};
+    int y {20};
+    int* ip;
+    ip=&x;
+    cout<< "ip: " << ip << endl; //Address of x is printed
+    cout<< "*ip: " << *ip << endl;  //Value of x is printed
+    cout<< "x: " << x << endl;
+    cout<< "y: " << y << endl;  
+//references
+    int n {29};
+    int& k=n;
+    cout<< "n: " << n << endl;
+    cout<< "k: " << k << endl;
+
+    k=15;
+    cout<< "k: " << k << endl;
+    cout<< "n: " << n << endl;
+/*If value of reference is changed, 
+the value of the variable it references also changes. 
+This is because a reference is an alias for the variable it references.*/
 }
